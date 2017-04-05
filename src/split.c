b774b6ccfc14a128022171a730cc2062f8b00d86
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-06 00:41:43 +0200

    Try to fix Window

--- src/split.c
+++ src/split.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <stdint.h>
 
+#ifdef _WIN32
+void * my_memmem(const void *l, size_t l_len, const void *s, size_t s_len);
+#else
+#define my_memmem memmem
+#endif
+
 //split by first CRLF
 SEXP split_header(unsigned char * haystack, size_t n){
   SEXP out = PROTECT(allocVector(VECSXP, 2));
@@ -40,7 +46,7 @@ SEXP R_split_boundary(SEXP body, SEXP boundary){
   //find the needles
   int count = 0;
   unsigned char * cur = NULL;
-  for(count = 0; (cur = memmem(haystack, n, needle, m)); count++){
+  for(count = 0; (cur = my_memmem(haystack, n, needle, m)); count++){
     offsets[count] = cur;
     haystack = cur + m;
   }
@@ -82,3 +88,33 @@ SEXP R_unquote(SEXP string){
     return ScalarString(mkCharLen(str + 1, len - 2));
   return string;
 }
+
+
+#ifdef _WIN32
+void * my_memmem(const void *l, size_t l_len, const void *s, size_t s_len)  {
+  register char *cur, *last;
+  const char *cl = (const char *)l;
+  const char *cs = (const char *)s;
+
+  /* we need something to compare */
+  if (l_len == 0 || s_len == 0)
+    return NULL;
+
+  /* "s" must be smaller or equal to "l" */
+  if (l_len < s_len)
+    return NULL;
+
+  /* special case where s_len == 1 */
+  if (s_len == 1)
+    return memchr(l, (int)*cs, l_len);
+
+  /* the last position where its possible to find "s" in "l" */
+  last = (char *)cl + l_len - s_len;
+
+  for (cur = (char *)cl; cur <= last; cur++)
+    if (cur[0] == cs[0] && memcmp(cur, cs, s_len) == 0)
+      return cur;
+
+    return NULL;
+}
+#endif
