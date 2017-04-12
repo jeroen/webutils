72c55d12572a2d5241d0f351a1ade33ee4f84fee
Jeroen Ooms <jeroenooms@gmail.com>
2017-04-12 12:50:54 +0200

    Update memmem fallback

--- src/split.c
+++ src/split.c
@@ -3,14 +3,13 @@
 #include <string.h>
 #include <stdint.h>
 
-#if !defined(_WIN32) && !defined(__sun)
-#define HAVE_MEMMEM
-#endif
+//from memmem.c
+void * fallback_memmem(const void *h0, size_t k, const void *n0, size_t l);
 
-#ifdef HAVE_MEMMEM
+#if !defined(_WIN32) && !defined(__sun)
 #define my_memmem memmem
 #else
-void * my_memmem(const void *l, size_t l_len, const void *s, size_t s_len);
+#define my_memmem fallback_memmem
 #endif
 
 //split by first CRLF
@@ -93,33 +92,3 @@ SEXP R_unquote(SEXP string){
     return ScalarString(mkCharLen(str + 1, len - 2));
   return string;
 }
-
-
-#ifndef HAVE_MEMMEM
-void * my_memmem(const void *l, size_t l_len, const void *s, size_t s_len)  {
-  register char *cur, *last;
-  const char *cl = (const char *)l;
-  const char *cs = (const char *)s;
-
-  /* we need something to compare */
-  if (l_len == 0 || s_len == 0)
-    return NULL;
-
-  /* "s" must be smaller or equal to "l" */
-  if (l_len < s_len)
-    return NULL;
-
-  /* special case where s_len == 1 */
-  if (s_len == 1)
-    return memchr(l, (int)*cs, l_len);
-
-  /* the last position where its possible to find "s" in "l" */
-  last = (char *)cl + l_len - s_len;
-
-  for (cur = (char *)cl; cur <= last; cur++)
-    if (cur[0] == cs[0] && memcmp(cur, cs, s_len) == 0)
-      return cur;
-
-    return NULL;
-}
-#endif
