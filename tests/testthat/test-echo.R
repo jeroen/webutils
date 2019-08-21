008a147df5fe869edd88612872de5ccfe2ff55ff
Jeroen Ooms <jeroenooms@gmail.com>
2019-08-21 12:11:04 +0200

    Make unit test faster on Windows

--- tests/testthat/test-echo.R
+++ tests/testthat/test-echo.R
@@ -41,8 +41,12 @@ test_that("Echo a big file", {
   # Create a random file (~30 MB)
   # Note: can test even bigger files but curl_echo() is a bit slow on Windows
   tmp <- tempfile()
-  n <- runif(1, 3e6, 4e6)
-  buf <- serialize(rnorm(n), NULL)
+  size <- if(.Platform$OS.type == "windows"){
+    1e5
+  } else {
+    runif(1, 3e6, 4e6)
+  }
+  buf <- serialize(rnorm(size), NULL)
   writeBin(buf, tmp)
   on.exit(unlink(tmp))
 
