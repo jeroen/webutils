835953ea30de789293082f7f4c1c0f45566fae21
Jeroen Ooms <jeroenooms@gmail.com>
2020-04-28 22:16:05 +0200

    Allow charset in multipart Content-Type Closes #3

--- R/get_boundary.R
+++ R/get_boundary.R
@@ -1,7 +1,9 @@
 get_boundary <- function(content_type){
-  # Check for multipart
-  if(!grepl("multipart/form-data; boundary=", content_type, fixed = TRUE))
+  # Check for multipart header
+  if(!grepl("multipart/form-data;", content_type, fixed = TRUE))
     stop("Content type is not multipart/form-data: ", content_type)
+  if(!grepl("boundary=", content_type, fixed = TRUE))
+    stop("Multipart content-type header without boundary: ", content_type)
 
   # Extract bounary
   m <- regexpr('boundary=[^; ]{2,}', content_type, ignore.case = TRUE)
